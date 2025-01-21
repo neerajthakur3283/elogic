89ce29e7a05bf46eb7666c953edb0d302ab63eec
Akash <110659040+Akash270901@users.noreply.github.com>
2025-01-21 10:03:04 +0530

    mt

--- src/app/our-industrial-solutions/page.tsx
+++ src/app/our-industrial-solutions/page.tsx
@@ -1,15 +1,14 @@
-import Link from 'next/link'
-import React from 'react'
-import CardApps from './CardApps';
-import PageBanner from './PageBanner';
-import YouAre from './_components/YouAre';
-import Industries from './_components/Industries';
-import HardwareEcosystem from './_components/HardwareEcosystem';
-import OurProcess from './_components/OurProcess';
+import React from "react";
+import CardApps from "./CardApps";
+import PageBanner from "./PageBanner";
+import YouAre from "./_components/YouAre";
+import Industries from "./_components/Industries";
+import HardwareEcosystem from "./_components/HardwareEcosystem";
+import OurProcess from "./_components/OurProcess";
 
 function page() {
   return (
-    <>      
+    <>
       <PageBanner />
       <CardApps />
       <Industries />
@@ -17,7 +16,7 @@ function page() {
       <HardwareEcosystem />
       <OurProcess />
     </>
-  )
+  );
 }
 
-export default page
+export default page;
