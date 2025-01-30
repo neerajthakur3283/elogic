e73c00dd2f55474a3a15508d353d2198944288cb
kapiluniqueit <kapil.uniqueit@gmail.com>
2025-01-30 18:59:57 +0530

    about us page responsiveness is pending

--- src/app/about-us/_components/RoadMap.tsx
+++ src/app/about-us/_components/RoadMap.tsx
@@ -47,12 +47,12 @@ const RoadMap = () => {
   return (
     <>
         <section>
-            <div className="w-full overflow-hidden">
+            <div className="w-full overflow-hidden 2xl:pb-[190px] xl:pb-[75px] lg:pb-[66px] md:pb-[45px] pb-[50px]">
                 <div className="flex justify-center">
                     <span className="bg-[rgba(53,99,233,0.2)] 2xl:rounded-[16px] xl:rounded-[13.85px] md:rounded-[12.1px] rounded-[7.48px] text-[#3563E9] font-semibold 2xl:text-[20px] xl:text-[15px] md:text-[13.33px] text-[15.24px] 2xl:px-[24px] xl:px-[18px] px-[16px] 2xl:h-[52px] xl:h-[39px] md:h-[35px] h-[40.29px] flex items-center justify-center">Roadmap</span>
                 </div>
                 <h2 className="text-center 2xl:mt-[20px] mt-[15px] 2xl:text-[36px] xl:text-[27px] md:text-[24px] text-[22.45px] font-bold text-black">Our Journey</h2>
-                <div className="2xl:mt-[60px] xl:mt-[40px] mt-[30px] min-[1770px]:max-w-[1333px] mx-auto relative 2xl:pt-[145px] [&_.step-card:nth-child(1)>.step-dot]:-left-[151px] [&_.step-card:nth-child(3)>.step-dot]:-left-[155px] [&_.step-card:nth-child(5)>.step-dot]:-left-[155px] [&_.step-card:nth-child(2)>.step-dot]:-right-[151px] [&_.step-card:nth-child(1)]:mb-[63px] [&_.step-card:nth-child(2)]:mb-[50px] [&_.step-card:nth-child(3)]:mb-[48px] [&_.step-card:nth-child(4)]:mb-[47px] [&_.step-card:nth-child(5)]:mb-[75px]">
+                <div className="2xl:mt-[60px] xl:mt-[40px] mt-[30px] max-w-[1333px] max-[1535px]:max-w-[1230px] mx-auto relative xl:pt-[135px] [&_.step-card:nth-child(odd)>.step-dot]:-left-[125px] [&_.step-card:nth-child(even)>.step-dot]:-right-[125px] [&_.step-card:nth-child(1)]:mb-[39px] [&_.step-card:nth-child(2)]:mb-[40px] [&_.step-card:nth-child(3)]:mb-[36px] [&_.step-card:nth-child(4)]:mb-[46px] [&_.step-card:nth-child(5)]:mb-[45px] [&_.step-card:nth-child(1)>_.step-line]:after:top-[96px] [&_.step-card:nth-child(3)>_.step-line]:after:top-[105px] [&_.step-card:nth-child(5)>_.step-line]:after:top-[96px] 2xl:[&_.step-card:nth-child(even)>_.step-line]:after:top-[105px] xl:[&_.step-card:nth-child(even)>_.step-line]:after:top-[99px] [&_.step-card:nth-child(odd)>_.step-line]:after:left-[-125px] [&_.step-card:nth-child(even)>_.step-line]:after:right-[-125px]">
                     {timelineData.map((item, index) => (
                     <div
                         key={index}
@@ -60,18 +60,18 @@ const RoadMap = () => {
                         index % 2 === 1 ? "flex-row" : "flex-row-reverse"
                         } `}
                     >
-                        <div className="2xl:w-[421px] rounded-[13px_13px_0_13px] overflow-hidden 2xl:shadow-[0_0_22px_0_rgba(0,0,0,0.25)] xl:shadow-[0_0_17px_0_rgba(0,0,0,0.25)] md:shadow-[0_0_14px_0_rgba(0,0,0,0.25)] shadow-[0_0_13px_0_rgba(0,0,0,0.25)]">
-                        <div>
-                            <Image src={item.image} alt={item.year} />
-                            <div className="2xl:p-[24px]">
+                        <div className="step-line xl:ps-[14px] xl:pt-[14px] xl:pe-[14px] bg-no-repeat bg-[url('../images/step-card-bg.svg')] 2xl:pb-[10px] xl:pb-[25px] relative after:absolute after:w-[300px] after:h-[3px] after:bg-[#3563E9] after:z-[-1]">
+                        <div className="xl:w-[422px]">
+                            <Image className="rounded-[13px_13px_0_0]" src={item.image} alt={item.year} />
+                            <div className="2xl:p-[24px] xl:p-[20px]">
                                 <h3 className="text-black font-bold 2xl:text-[24px] xl:text-[18px] text-base">{item.year}</h3>
                                 <p className="text-black text-[15px] mt-[15px]">{item.description}</p>
                             </div>
                         </div>
                         </div>
                         <div className="relative step-dot">
-                        <div className="2xl:w-[62px] 2xl:h-[62px] bg-[rgba(53,99,233,0.2)] rounded-full absolute top-1/2 transform -translate-y-[60px] -translate-x-1/2 flex justify-center items-center">
-                            <div className="bg-[#3563E9] 2xl:w-[24px] 2xl:h-[24px] rounded-full">
+                        <div className="2xl:w-[62px] xl:w-[55px] 2xl:h-[62px] xl:h-[55px] bg-[rgba(53,99,233,0.2)] rounded-full absolute top-1/2 transform -translate-y-[70px] -translate-x-1/2 flex justify-center items-center">
+                            <div className="bg-[#3563E9] 2xl:w-[24px] xl:w-[20px] 2xl:h-[24px] xl:h-[20px] rounded-full">
 
                             </div>
                         </div>
@@ -82,7 +82,7 @@ const RoadMap = () => {
                     <div className="absolute text-[#3563E9] -top-[17px]">
                     <TbHexagonFilled className="2xl:w-[30px] 2xl:h-[30px] rotate-90" />
                     </div>
-                    <Image src={dottedLine} alt="" />
+                    <Image src={dottedLine} className="max-[1535px]:w-[185px]" alt="" />
                     </div>
                 </div>
             </div>
