d926ceadd04de23d9d04fd7fe016fcddc30d245f
kapiluniqueit <kapil.uniqueit@gmail.com>
2025-02-11 16:26:20 +0530

    services heading updated

--- src/app/services-overview/human-machine-interface-ux-development/_components/PageBanner.tsx
+++ src/app/services-overview/human-machine-interface-ux-development/_components/PageBanner.tsx
@@ -11,8 +11,7 @@ function PageBanner() {
           <div className="2xl:py-[260px] xl:py-[195px] flex items-center justify-center md:py-[173px] py-[185px] 2xl:px-[72px] xl:px-[54px] lg:px-[48px] px-[20px] absolute left-0 top-0 w-full h-full">
             <div className="flex justify-center text-white flex-col text-center 2xl:gap-y-[40px] xl:gap-y-[30px] md:gap-y-[26.67px] gap-y-[16px]">
               <h1 className="2xl:text-[68.5px] xl:text-[49.35px] md:text-[48px] text-[32px] font-semibold">
-                Human Machine Interface<br/>
-              and UX Development
+              Human machine Interface<br/> and UX development
               </h1>
             </div>
           </div>
