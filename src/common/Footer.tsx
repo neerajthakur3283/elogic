3daf4b54a7edb6003998897f7bcf6f1bfbc15ce6
Akash <110659040+Akash270901@users.noreply.github.com>
2025-02-26 11:59:35 +0530

    mt

--- src/common/Footer.tsx
+++ src/common/Footer.tsx
@@ -1,11 +1,11 @@
-'use client'
+"use client";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 import { FaPhone } from "react-icons/fa6";
 import { FaEnvelope } from "react-icons/fa";
 import { motion } from "framer-motion";
-import { MdArrowOutward } from 'react-icons/md'
+import { MdArrowOutward } from "react-icons/md";
 
 // images
 import footerLogo from "@/src/assets/images/footer-logo.svg";
@@ -20,7 +20,8 @@ function Footer() {
           whileInView={{ opacity: 1, y: 0 }}
           viewport={{ once: true }}
           transition={{ duration: 0.8, delay: 0.3 }}
-          className="bg-[#172747] 2xl:px-[72px] xl:px-[54px] lg:px-[48px] px-[20px] 2xl:py-[100px] xl:py-[75px] lg:py-[66px] md:py-[45px] py-[50px] 2xl:rounded-ss-[100px] xl:rounded-ss-[75px] md:rounded-ss-[66px] rounded-ss-[40px] 2xl:rounded-se-[100px] xl:rounded-se-[75px] md:rounded-se-[66px] rounded-se-[40px] max-[767px]:pb-[30px]">
+          className="bg-[#172747] 2xl:px-[72px] xl:px-[54px] lg:px-[48px] px-[20px] 2xl:py-[100px] xl:py-[75px] lg:py-[66px] md:py-[45px] py-[50px] 2xl:rounded-ss-[100px] xl:rounded-ss-[75px] md:rounded-ss-[66px] rounded-ss-[40px] 2xl:rounded-se-[100px] xl:rounded-se-[75px] md:rounded-se-[66px] rounded-se-[40px] max-[767px]:pb-[30px]"
+        >
           <div className="flex items-start 2xl:gap-x-[85px] xl:gap-x-[60px] lg:gap-x-[70px] max-[1200px]:!gap-x-[40px] max-[991px]:!gap-x-[30px] max-[790px]:!gap-x-[20px] max-[991px]:justify-around max-[767px]:flex-wrap">
             <div className="w-full 2xl:max-w-[430px] xl:max-w-[322.5px] lg:max-w-[286.67px] max-[1200px]:max-w-[250.67px] max-[767px]:max-w-full">
               <Image
@@ -34,7 +35,9 @@ function Footer() {
               </div>
             </div>
             <div className="w-full 2xl:max-w-[218px] xl:max-w-[180px] lg:max-w-[155px] max-[1200px]:max-w-[94px] max-[767px]:max-w-full md:mt-0 mt-8">
-              <h4 className="mb-3 text-white font-bold 2xl:text-[18px] xl:text-[14.5px] md:text-[13.75px] text-[17px]">Industrial IoT Solutions</h4>
+              <h4 className="mb-3 text-white font-bold 2xl:text-[18px] xl:text-[14.5px] md:text-[13.75px] text-[17px]">
+                Industrial IoT Solutions
+              </h4>
               <ul className="flex flex-col xl:gap-y-[22px] lg:gap-y-[19px] md:gap-y-[17px] gap-y-[15px] 2xl:text-[14.5px] xl:text-[12px] md:text-[10.67px] tracking-[-0.4px]">
                 <li>
                   <Link href="/our-industrial-solutions" className="text-white">
@@ -42,17 +45,26 @@ function Footer() {
                   </Link>
                 </li>
                 <li>
-                  <Link href="/energy-consumption-monitoring" className="text-white">
+                  <Link
+                    href="/energy-consumption-monitoring"
+                    className="text-white"
+                  >
                     Energy Monitoring
                   </Link>
                 </li>
                 <li>
-                  <Link href="/machine-condition-monitoring" className="text-white">
+                  <Link
+                    href="/machine-condition-monitoring"
+                    className="text-white"
+                  >
                     Condition Monitoring
                   </Link>
                 </li>
                 <li>
-                  <Link href="/oee-and-production-monitoring" className="text-white">
+                  <Link
+                    href="/oee-and-production-monitoring"
+                    className="text-white"
+                  >
                     Productivity Monitoring
                   </Link>
                 </li>
@@ -69,7 +81,9 @@ function Footer() {
               </ul>
             </div>
             <div className="w-full 2xl:max-w-[270px] xl:max-w-[215px] md:max-w-[190px] md:my-0 my-8">
-              <h4 className="mb-3 text-white font-bold 2xl:text-[18px] xl:text-[14.5px] md:text-[13.75px] text-[17px]">Product Development</h4>
+              <h4 className="mb-3 text-white font-bold 2xl:text-[18px] xl:text-[14.5px] md:text-[13.75px] text-[17px]">
+                Product Development
+              </h4>
               <ul className="flex flex-col xl:gap-y-[22px] lg:gap-y-[19px] md:gap-y-[17px] gap-y-[15px] 2xl:text-[14.5px] xl:text-[12px] md:text-[10.67px] tracking-[-0.4px]">
                 <li>
                   <Link href="/services-overview" className="text-white">
@@ -77,51 +91,76 @@ function Footer() {
                   </Link>
                 </li>
                 <li>
-                  <Link href="/services-overview/embedded-hardware-design" className="text-white">
+                  <Link
+                    href="/services-overview/embedded-hardware-design"
+                    className="text-white"
+                  >
                     Embedded Hardware Design
                   </Link>
                 </li>
                 <li>
-                  <Link href="/services-overview/embedded-software-design" className="text-white">
+                  <Link
+                    href="/services-overview/embedded-software-design"
+                    className="text-white"
+                  >
                     Embedded Software Design
                   </Link>
                 </li>
                 <li>
-                  <Link href="/services-overview/human-machine-interface-ux-development" className="text-white">
+                  <Link
+                    href="/services-overview/human-machine-interface-ux-development"
+                    className="text-white"
+                  >
                     Human-machine Interface Development
                   </Link>
                 </li>
                 <li>
-                  <Link href="/services-overview/wireless-systems-development" className="text-white">
+                  <Link
+                    href="/services-overview/wireless-systems-development"
+                    className="text-white"
+                  >
                     Wireless Systems Development
                   </Link>
                 </li>
                 <li>
-                  <Link href="/services-overview/mechanical-and-industrial-design" className="text-white">
+                  <Link
+                    href="/services-overview/mechanical-and-industrial-design"
+                    className="text-white"
+                  >
                     Mechanical & Industrial Design
                   </Link>
                 </li>
                 <li>
-                  <Link href="/services-overview/manufacturing-optimization-support" className="text-white">
+                  <Link
+                    href="/services-overview/manufacturing-optimization-support"
+                    className="text-white"
+                  >
                     Manufacturing Support
                   </Link>
                 </li>
                 <li>
-                  <Link href="/services-overview/custom-iot-apps-development" className="text-white">
+                  <Link
+                    href="/services-overview/custom-iot-apps-development"
+                    className="text-white"
+                  >
                     IoT application Development
                   </Link>
                 </li>
                 <li>
-                  <Link href="/services-overview/technology-compliance-consulting" className="text-white">
+                  <Link
+                    href="/services-overview/technology-compliance-consulting"
+                    className="text-white"
+                  >
                     Technology Consulting
                   </Link>
                 </li>
               </ul>
             </div>
             <div className="w-full 2xl:max-w-[155px] xl:max-w-[140px] md:max-w-[105px] max-[767px]:flex max-[767px]:justify-between max-[480px]:flex-wrap max-[480px]:flex-col max-[480px]:gap-[30px]">
-
               <div>
-                <h4 className="mb-3 text-white font-bold 2xl:text-[18px] xl:text-[14.5px] md:text-[13.75px] text-[17px]">Products</h4>
+                <h4 className="mb-3 text-white font-bold 2xl:text-[18px] xl:text-[14.5px] md:text-[13.75px] text-[17px]">
+                  Products
+                </h4>
                 <ul className="flex flex-col xl:gap-y-[22px] lg:gap-y-[19px] md:gap-y-[17px] gap-y-[15px] 2xl:text-[14.5px] xl:text-[12px] md:text-[10.67px] tracking-[-0.4px]">
                   <li>
                     <Link href="/blogs" className="text-white">
@@ -137,7 +176,9 @@ function Footer() {
               </div>
 
               <div className="md:my-6">
-                <h4 className="mb-3 text-white font-bold 2xl:text-[18px] xl:text-[14.5px] md:text-[13.75px] text-[17px]">About Us</h4>
+                <h4 className="mb-3 text-white font-bold 2xl:text-[18px] xl:text-[14.5px] md:text-[13.75px] text-[17px]">
+                  About Us
+                </h4>
                 <ul className="flex flex-col xl:gap-y-[22px] lg:gap-y-[19px] md:gap-y-[17px] gap-y-[15px] 2xl:text-[14.5px] xl:text-[12px] md:text-[10.67px] tracking-[-0.4px]">
                   <li>
                     <Link href="/about-us" className="text-white">
@@ -163,7 +204,9 @@ function Footer() {
               </div>
 
               <div>
-                <h4 className="mb-3 text-white font-bold 2xl:text-[18px] xl:text-[14.5px] md:text-[13.75px] text-[17px]">Media</h4>
+                <h4 className="mb-3 text-white font-bold 2xl:text-[18px] xl:text-[14.5px] md:text-[13.75px] text-[17px]">
+                  Media
+                </h4>
                 <ul className="flex flex-col xl:gap-y-[22px] lg:gap-y-[19px] md:gap-y-[17px] gap-y-[15px] 2xl:text-[14.5px] xl:text-[12px] md:text-[10.67px] tracking-[-0.4px]">
                   <li>
                     <Link href="/blogs" className="text-white">
@@ -177,7 +220,6 @@ function Footer() {
                   </li>
                 </ul>
               </div>
-
             </div>
             <div className="w-full 2xl:max-w-[230px] xl:max-w-[172.5px] md:max-w-[153.33px] 2xl:text-[14px] xl:text-[10.5px] md:text-[9.33px]  text-white flex flex-col gap-y-[25px] xl:gap-y-[21px] lg:gap-y-[19px] md:gap-y-[16px] md:mt-0 mt-8">
               <span className="capitalize">Contact us</span>
@@ -222,8 +264,12 @@ function Footer() {
                   {/* <span className="flex w-[1px] bg-[#747D91] 2xl:h-[20px] xl:h-[15px] md:h-[13px] 2xl:mx-[32px] xl:mx-[24px] lg:mx-[21px] md:mx-[18px]"></span> */}
                 </Link>
                 <div className="sm:ms-5 ms-3">
-                  <Link className="bg-[#3563E9] text-white px-[2] w-full 2xl:min-w-[275px] xl:min-w-[209px] md:min-w-[175.44px] sm:min-w-[195px] min-w-[175px] 2xl:h-[52px] xl:h-[39px] md:h-[35px] h-[40.29px] flex justify-center items-center py-[2px] 2xl:text-[20px] xl:text-[15px] md:text-[13.33px] text-[15.24px] md:font-bold font-semibold rounded-[16px] capitalize duration-500 hover:bg-white hover:text-black"
-                    href="/contact-us">Schedule a demo <MdArrowOutward className="ms-2.5" /></Link>
+                  <Link
+                    className="bg-[#3563E9] text-white px-[2] w-full 2xl:min-w-[275px] xl:min-w-[209px] md:min-w-[175.44px] sm:min-w-[195px] min-w-[175px] 2xl:h-[52px] xl:h-[39px] md:h-[35px] h-[40.29px] flex justify-center items-center py-[2px] 2xl:text-[20px] xl:text-[15px] md:text-[13.33px] text-[15.24px] md:font-bold font-semibold rounded-[16px] capitalize duration-500 hover:bg-white hover:text-black"
+                    href="/contact-us"
+                  >
+                    Schedule a demo <MdArrowOutward className="ms-2.5" />
+                  </Link>
                 </div>
               </div>
               <div className="text-white font-medium 2xl:text-[14.5px] xl:text-[12px] md:text-[10.67px]">
