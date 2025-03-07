627b38b2c8f0d2726a8e73b732ca02c1b2c1e598
Neeraj Kaushal <neerajthakur.3283@gmail.com>
2025-03-07 16:18:49 +0530

    remove blinking effect

--- src/components/_home/Slider.tsx
+++ src/components/_home/Slider.tsx
@@ -2,7 +2,7 @@
 import Link from "next/link";
 import Image from "next/image";
 import React, { useEffect, useState, useRef } from "react";
-import { motion } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
 
 // images
 import slideOne from "@/src/assets/images/home-slide-1.png";
@@ -56,7 +56,7 @@ const Slider: React.FC = () => {
     const startSlider = () => {
       intervalRef.current = setInterval(() => {
         setCurrentIndex((prevIndex) => (prevIndex + 1) % sliderData.length);
-      }, 3000);
+      }, 4000);
     };
 
     startSlider();
@@ -74,11 +74,12 @@ const Slider: React.FC = () => {
 
   return (
     <section>
-      <motion.div 
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1}}
-      transition={{ duration: 0.8, ease: "easeInOut", delay: 0}}
-      className="relative ms:pt-0 pt-4 after:absolute after:left-0 after:right-0 after:top-0 after:h-[210px] after:bg-gradient-to-b after:from-black to-[rgba(0,0,0,0)]">
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 0.8, ease: "easeInOut", delay: 0 }}
+        className="relative ms:pt-0 pt-4 after:absolute after:left-0 after:right-0 after:top-0 after:h-[210px] after:bg-gradient-to-b after:from-black to-[rgba(0,0,0,0)]"
+      >
         <div className="absolute z-[2] left-0 top-0 bottom-0 flex items-center w-full">
           <div className="relative w-full" key={currentIndex}>
             <div className="slider-image w-full relative flex items-center md:flex-nowrap flex-wrap justify-between">
@@ -87,9 +88,18 @@ const Slider: React.FC = () => {
                   <h1 className="text-white 2xl:text-[60px] xl:text-[45px] text-[40px] md:font-bold font-semibold 2xl:w-[550px] xl:w-[450px] md:w-[355px] mb-3 tracking-[-1.5px] max-[767px]:leading-[1.34]">
                     Digitize your shop floor operations
                   </h1>
-                  <p className="text-[#F9F9FB] 2xl:text-[24px] xl:text-[18px] text-base md:leading-7 leading-[1.65] tracking-[-0.2px] 2xl:pt-[30px] xl:pt-[20px] md:pt-[15x] pt-[12px]">
-                    {sliderData[currentIndex].slidePara}
-                  </p>
+                  <AnimatePresence mode="wait">
+                    <motion.p
+                      key={currentIndex}
+                      initial={{ opacity: 0, y: 10 }}
+                      animate={{ opacity: 1, y: 0 }}
+                      exit={{ opacity: 1, y: -10 }}
+                      transition={{ duration: 1, ease: "easeInOut" }}
+                      className="text-[#F9F9FB] 2xl:text-[24px] xl:text-[18px] text-base md:leading-7 leading-[1.65] tracking-[-0.2px] 2xl:pt-[30px] xl:pt-[20px] md:pt-[15x] pt-[12px]"
+                    >
+                      {sliderData[currentIndex].slidePara}
+                    </motion.p>
+                  </AnimatePresence>
                   <div className="2xl:pt-[40px] xl:pt-[30px] md:pt-[26px] pt-[20px]">
                     <Link
                       className="bg-[#3563E9] text-white px-[2] w-full 2xl:max-w-[200px] xl:max-w-[153.5px] md:max-w-[136.44px] max-w-[123px] 2xl:h-[60px] xl:h-[45px] md:h-[41px] h-[48px] flex justify-center items-center py-[2px] 2xl:text-[19px] xl:text-[14.25px] text-[16px] md:text-[12.67px] md:font-bold font-semibold rounded-[16px] capitalize duration-500 hover:bg-white hover:text-black"
@@ -101,15 +111,23 @@ const Slider: React.FC = () => {
                 </div>
               </div>
               <div className="2xl:w-[calc(100%_-_845px)] xl:w-[calc(100%_-_632px)] md:w-[calc(100%_-_562px)] slide-img max-[767px]:px-[20px] md:pt-0 sm:pt-[90px] pt-3 2xl:px-[85px] min-[1800px]:px-[135px] xl:px-[80px] md:px-[30px] max-[991px]:!px-[20px] max-[991px]:!w-[425px] max-[767px]:!w-auto">
-                <div className="">
-                  <Image
-                    className="w-full"
-                    src={sliderData[currentIndex].sliderImg}
-                    alt=""
-                    width={1920}
-                    height={957}
-                  />
-                </div>
+                <AnimatePresence mode="wait">
+                  <motion.div
+                    key={currentIndex}
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                    exit={{ opacity: 1 }}
+                    transition={{ duration: 1, ease: "easeInOut" }}
+                  >
+                    <Image
+                      className="w-full"
+                      src={sliderData[currentIndex].sliderImg}
+                      alt=""
+                      width={1920}
+                      height={957}
+                    />
+                  </motion.div>
+                </AnimatePresence>
               </div>
             </div>
           </div>
